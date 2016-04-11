9f5cc1eb65ac95faa970152b21de9fdfa3f07a96
maggankoolman <lindermag@hotmail.com>
2016-04-11 13:05:34 +0200

    Obj klart Fuck VAO

--- TestSprite.cpp
+++ TestSprite.cpp
@@ -3,6 +3,7 @@
 
 struct Vertex {
 	float x, y, z;
+	float u, v;
 	GLbyte r, g, b;
 };
 
@@ -59,12 +60,28 @@ void TestSprite::init(float x, float y, float width, float height) {
 		vertexData[i].r = 200;
 		vertexData[i].g = 120;
 		vertexData[i].b = 80;
+		vertexData[i].u = 0;
+		vertexData[i].v = 1;
 	}
 
+	/*glBindBuffer(GL_ARRAY_BUFFER, _vboID);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertexData), vertexData, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);*/
+
+	glGenVertexArrays(1, &mVertexArray);
+	glBindVertexArray(mVertexArray);
+
+	glGenBuffers(1, &_vboID);
 	glBindBuffer(GL_ARRAY_BUFFER, _vboID);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertexData), vertexData, GL_STATIC_DRAW);
 
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)offsetof(Vertex, x));
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, (void*)offsetof(Vertex, u));
+	glVertexAttribPointer(2, 3, GL_UNSIGNED_BYTE, GL_TRUE, 0, (void*)offsetof(Vertex, r));
+
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
 
 	worldMat = glm::mat4x4(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1);
 }
@@ -74,11 +91,14 @@ void TestSprite::draw(const GLuint &shaderProg) {
 	GLuint world = glGetUniformLocation(shaderProg, "world");
 	glUniformMatrix4fv(world, 1, GL_FALSE, &this->worldMat[0][0]);
 
+	//glBindVertexArray(mVertexArray);
 	glBindBuffer(GL_ARRAY_BUFFER, _vboID);
-
+	
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, x));
-	glVertexAttribPointer(1, 3, GL_UNSIGNED_BYTE, GL_TRUE, sizeof(Vertex), (void*)offsetof(Vertex, r));
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, u));
+	glVertexAttribPointer(2, 3, GL_UNSIGNED_BYTE, GL_TRUE, sizeof(Vertex), (void*)offsetof(Vertex, r));
 	glDrawArrays(GL_TRIANGLES, 0, 6);
 
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
 }
--- main.cpp
+++ main.cpp
@@ -36,8 +36,8 @@ int main(int argc, char** argv) {
 	Input input;
 	Game game;
 
-	FbxImport fbx;
-	fbx.initializeImporter();
+	//FbxImport fbx;
+	//fbx.initializeImporter();
 
 	while (running)
 	{
